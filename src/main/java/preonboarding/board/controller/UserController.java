d78e184eb86864e7825ec2519dbe48e0d15c2a45
Seongju <lsj40413@gmail.com>
2023-08-10 00:13:19 +0900

    feat(user): Presentation Layer
    - Controller를 구현하고, Token값 전달을 위해 Token이란 DTO를 선언.
    -> Token: String 타입에 비해 타입 안정성을 보장하고, 확장성을 고려해서 Token DTO 생성

---
+++ src/main/java/preonboarding/board/controller/UserController.java
@@ -0,0 +1,41 @@
+package preonboarding.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import preonboarding.board.domain.user.dto.Token;
+import preonboarding.board.domain.user.dto.UserLoginRequest;
+import preonboarding.board.domain.user.dto.UserSignUpRequest;
+import preonboarding.board.domain.user.service.UserService;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<String> signUp(
+            @RequestBody @Valid UserSignUpRequest signUpRequest
+    ) {
+        Long savedUserId = userService.signUp(signUpRequest);
+        String resourceUrl = String.format("/users/%s", savedUserId);
+        URI location = URI.create(resourceUrl);
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Token> login(
+            @RequestBody @Valid UserLoginRequest loginRequest
+    ) {
+        String token = userService.login(loginRequest);
+        return ResponseEntity.ok(new Token(token));
+    }
+}
---
+++ src/main/java/preonboarding/board/domain/user/dto/Token.java
@@ -0,0 +1,10 @@
+package preonboarding.board.domain.user.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Token {
+    private final String token;
+}
