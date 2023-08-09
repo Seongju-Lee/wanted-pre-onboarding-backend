445861195820bc850422ea792f6ccdcc41579323
Seongju <lsj40413@gmail.com>
2023-08-10 01:10:12 +0900

    refactor(Exception): 이메일 중복관련 custom예외 정의
    - 회원 가입 시, 이메일 중복이란 예외를 명시하기 위해 별도로 정의
    DuplicateKeyException보다 명확할 것으로 판단.

---
+++ src/main/java/preonboarding/board/global/exception/custom/DuplicateEmailException.java
@@ -0,0 +1,9 @@
+package preonboarding.board.global.exception.custom;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class DuplicateEmailException extends DataIntegrityViolationException {
+    public DuplicateEmailException() {
+        super("중복된 이메일이 존재합니다.");
+    }
+}
