7172b69c17a74804cc8baeb5f78e8337fd55ed23
Seongju <lsj40413@gmail.com>
2023-08-09 20:17:14 +0900

    feat(user): 요청 DTO 생성
    - 회원가입과 로그인에 따른 DTO 생성
    동일한 값과 검증로직을 거치지만 명시적으로 별도의 DTO생성

---
+++ src/main/java/preonboarding/board/domain/user/dto/UserLoginRequest.java
@@ -0,0 +1,24 @@
+package preonboarding.board.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequest {
+
+    private static final String EMAIL_VALIDATION_PATTERN = "^(.+)@(.+)$";
+
+    @NotBlank
+    @Pattern(regexp = EMAIL_VALIDATION_PATTERN, message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    private String password;
+}
